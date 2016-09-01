bc145e7afb0f1025ed9fd9643f1c80ca046b6cec
Carlos Diaz <ca.diaz0529@gmail.com>
2016-09-01 02:24:55 -0500

    Cambiar nombre de "projects" a "talleres"

--- projects/mysite/taller1/apps.py
+++
@@ -1,7 +0,0 @@
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class Taller1Config(AppConfig):
-    name = 'taller1'
--- projects/mysite/taller1/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    lista_prueba = [1, 2, 3, 4, 5]
-    context = {'lista_prueba': lista_prueba}
-    return render(request, 'taller1/index.html', context)
